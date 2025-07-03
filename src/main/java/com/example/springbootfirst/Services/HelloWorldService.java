5a756a5eb7f79fa206df95ff1a0afca1917138f7
Guruvel13 <letters2guruvel@gmail.com>
2025-07-03 12:57:48 +0530

    DAY-2 FN

---
+++ src/main/java/com/example/springbootfirst/Services/HelloWorldService.java
@@ -0,0 +1,10 @@
+package com.example.springbootfirst.Services;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class HelloWorldService {
+    public String helloworld(){
+        return "HelloWorld";
+    }
+}
--- src/main/java/com/example/springbootfirst/SpringbootfirstApplication.java
+++ src/main/java/com/example/springbootfirst/SpringbootfirstApplication.java
@@ -9,5 +9,5 @@ public class SpringbootfirstApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(SpringbootfirstApplication.class, args);
 	}
-
 }
+
